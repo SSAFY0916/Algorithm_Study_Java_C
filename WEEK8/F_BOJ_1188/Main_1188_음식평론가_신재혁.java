6bd225a74bfd8870c6ed7f1cd32d7433dcc4e93c
신재혁 <y_spk@naver.com>
2023-04-04 12:55:09 +0900

    [BOJ] 1963 소수경로 [BOJ] 10942 팰린드롬?

--- WEEK8/F_BOJ_1188/Main_1188_음식평론가_신재혁.java
+++ WEEK8/F_BOJ_1188/Main_1188_음식평론가_신재혁.java
@@ -10,16 +10,11 @@ public class Main_1188_음식평론가_신재혁 {
         int longSausage = sausages * people;
         int countCollisions = 0;
         Set<Integer> S = new TreeSet<>();
-        for(int i = people; i < longSausage; i+=people){
+        for(int i = people; i < longSausage; i+=people)
             S.add(i);
-        }
-        for(int i = sausages; i < longSausage; i+=sausages){
-            if(S.contains(i)){
+        for(int i = sausages; i < longSausage; i+=sausages)
+            if(S.contains(i))
                 countCollisions++;
-            }else{
-                S.add(i);
-            }
-        }
         System.out.println((people-1)-countCollisions);
     }
 }
