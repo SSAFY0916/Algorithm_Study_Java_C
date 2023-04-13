f70f0dd6aa597ede5215240951db69a6b6686293
신재혁 <y_spk@naver.com>
2023-04-13 15:41:43 +0900

    [BOJ] 1052 물병 수정...

--- WEEK9/A_BOJ_1052/Main_1052_물병_신재혁.java
+++ WEEK9/A_BOJ_1052/Main_1052_물병_신재혁.java
@@ -4,35 +4,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_1052_물병_신재혁 {
-    static int [] arr;
+    static int N, K, bitCount;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[N];
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        // 정수 N의 1 비트가 K개 이내가 되기 위해 더해야하는 수의 최소값은?
+        int ans = 0;
+        while(true){
+            // 일단 현재 N의 1 비트 수를 세어야한다
+            countBits();
+            if(bitCount <= K)
+                break;
+            // 비트 수가 충분히 줄어들 때 까지 가장 낮은 비트를 더해야 한다. (펜윅트리에서 배운 내용과 비슷하다)
+            int lo = N & -N; // 가장 낮은 비트 (펜윅트리에서 배운거)
+            ans += lo;
+            N += lo; // 가장 낮은 비트를 더함
         }
-        int Q = Integer.parseInt(br.readLine());
-        for(int i = 0; i < Q; i++){
-            st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            sb.append(isPalindrome(S,E)).append('\n');
-        }
-        System.out.println(sb);
+        System.out.println(ans);
     }
-    public static int isPalindrome(int S, int E){
-        int first = S-1;
-        int last = E-1;
-        while(first < last){
-            if(arr[first] != arr[last]){
-                return 0;
-            }
-            first++;
-            last--;
+    static void countBits(){
+        int n = N;
+        bitCount = 0;
+        while(n > 0){
+            if((n & 1) != 0)
+                bitCount++;
+            n >>= 1;
         }
-        return 1;
     }
 }
